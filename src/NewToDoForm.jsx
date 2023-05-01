87e5cd4e0ecd4f7a5a70f7e0078dcffa43eaa88e
RoxanaEmiliaAdam <banu_emiliaroxana@yahoo.com>
2023-05-01 13:12:37 +0300

    initial

---
+++ src/NewToDoForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export function NewToDoForm({ addTodo }) {
+  const [newItem, setNewItem] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (newItem === "") return;
+    addTodo(newItem);
+    setNewItem("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label>New Item</label>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          type="text"
+          id="item"
+        />
+      </div>
+      <button className="btn">Add</button>
+    </form>
+  );
+}
